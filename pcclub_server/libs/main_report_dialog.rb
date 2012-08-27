51d4f189c30bd32fbff31392a9a1ab797f484cbb
yarik32 <yarik32@mail.ru>
2012-08-27 17:26:19 +0300

    Main_Report_Dialog changed

--- pcclub_server/libs/main_report_dialog.rb
+++ pcclub_server/libs/main_report_dialog.rb
@@ -16,8 +16,26 @@ class MainReportDialog < FXDialogBox
 
         btn_layout = FXHorizontalFrame.new(page_layout, :opts => LAYOUT_FILL_X)
         button_close = FXButton.new(btn_layout, "Close",:opts => FRAME_RAISED|FRAME_THICK|LAYOUT_FILL)
+        button_color = FXButton.new(btn_layout, "Change the color for background", :opts =>FRAME_RAISED|FRAME_THICK|LAYOUT_FILL)
         button_print = FXButton.new(btn_layout, "Print", :opts => FRAME_RAISED|FRAME_THICK|LAYOUT_FILL)
 
+        button_close.connect(SEL_COMMAND) do |sender, sel, event|
+          close
+        end
+
+        button_color.connect(SEL_COMMAND) do |sender, sel, event|
+          dialog = FXColorDialog.new(self, "Choose Background color")
+          dialog.rgba = FXRGB(255, 0, 0)
+          if dialog.execute != 0
+            self.backColor=dialog.rgba
+          end
+        end
+
+        button_print.connect(SEL_COMMAND) do |sender, sel, event|
+          printer = FXPrintDialog.new(self, "Choose a print settings", :x => 50, :y => 50)
+          printer.create
+          printer.show
+        end
     end
 
 end
